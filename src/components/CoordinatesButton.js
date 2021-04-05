0117e10ca04f52c46aa1ab92af212bd99d102e45
RSigmaSG <sg.comparch@gmail.com>
2021-04-05 17:58:34 -0400

    commit tree 352a82211fd0636ae11e3f2f202ffe65c107c193

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,23 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+
+    constructor(props) {
+        super()
+        this.coords = props.onReceiveCoordinates
+    
+    }
+
+    log = () => {
+        console.log([this.coords([e.clientX, e.clientY]))
+    }
+
+    render() {
+        return (
+        <div>
+            <button onClick={this.log}>Print coordinates: </button>
+        </div>
+        )
+    }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+export default class DelayedButton extends Component {
+
+    constructor(props) {
+        super()
+        this.coords = props.onReceiveCoordinates
+        
+    
+    }
+
+    log = () => {
+        console.log(this.coords)
+    }
+
+    render() {
+        return (
+        <button onClick={this.log}></button>
+        )
+    }
+}
